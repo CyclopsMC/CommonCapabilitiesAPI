a022d057c17ef461f22ba1a611af52f06940de61
Ruben Taelman <rubensworks@gmail.com>
2016-06-19 18:14:11 +0200

    Make the default wrench only usable on blocks

--- capability/wrench/DefaultWrench.java
+++ capability/wrench/DefaultWrench.java
@@ -1,15 +1,16 @@
 package org.cyclops.commoncapabilities.api.capability.wrench;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.RayTraceResult;
 
 /**
- * Default implementation of a {@link IWrench}.
+ * Default implementation of a {@link IWrench} that only applies to blocks.
  * @author rubensworks
  */
 public class DefaultWrench implements IWrench {
     @Override
     public boolean canUse(EntityPlayer player, WrenchTarget target) {
-        return true;
+        return target.getType() == RayTraceResult.Type.BLOCK;
     }
 
     @Override
