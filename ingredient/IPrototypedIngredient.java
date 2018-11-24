1876209033332acb188abedb79a744961e198f9c
Ruben Taelman <rubensworks@gmail.com>
2018-11-24 15:19:08 +0100

    Add serialization methods to IPrototypedIngredient

--- ingredient/IPrototypedIngredient.java
+++ ingredient/IPrototypedIngredient.java
@@ -1,5 +1,9 @@
 package org.cyclops.commoncapabilities.api.ingredient;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.util.Constants;
+
 /**
  * An ingredient that is identified by a given instance and can be matched with other instances under a given condition.
  *
@@ -25,5 +29,53 @@ public interface IPrototypedIngredient<T, M> extends Comparable<IPrototypedIngre
      * @return The condition under which the prototype instance can be matched.
      */
     public M getCondition();
+
+    /**
+     * Deserialize an ingredient to NBT.
+     * @param prototypedIngredient Ingredient.
+     * @return An NBT representation of the given ingredient.
+     */
+    public static <T, M> NBTTagCompound serialize(IPrototypedIngredient<T, M> prototypedIngredient) {
+        NBTTagCompound tag = new NBTTagCompound();
+
+        IngredientComponent<T, M> component = prototypedIngredient.getComponent();
+        tag.setString("ingredientComponent", component.getName().toString());
+
+        IIngredientSerializer<T, M> serializer = component.getSerializer();
+        tag.setTag("prototype", serializer.serializeInstance(prototypedIngredient.getPrototype()));
+        tag.setTag("condition", serializer.serializeCondition(prototypedIngredient.getCondition()));
+
+        return tag;
+    }
+
+    /**
+     * Deserialize an ingredient from NBT
+     * @param tag An NBT tag.
+     * @return A new ingredient instance.
+     * @throws IllegalArgumentException If the given tag is invalid or does not contain data on the given ingredient.
+     */
+    public static PrototypedIngredient deserialize(NBTTagCompound tag) throws IllegalArgumentException {
+        if (!tag.hasKey("ingredientComponent", Constants.NBT.TAG_STRING)) {
+            throw new IllegalArgumentException("Could not find a ingredientComponent entry in the given tag");
+        }
+        if (!tag.hasKey("prototype")) {
+            throw new IllegalArgumentException("Could not find a prototype entry in the given tag");
+        }
+        if (!tag.hasKey("condition")) {
+            throw new IllegalArgumentException("Could not find a condition entry in the given tag");
+        }
+
+        String componentName = tag.getString("ingredientComponent");
+        IngredientComponent<?, ?> component = IngredientComponent.REGISTRY.getValue(new ResourceLocation(componentName));
+        if (component == null) {
+            throw new IllegalArgumentException("Could not find the ingredient component type " + componentName);
+        }
+
+        IIngredientSerializer serializer = component.getSerializer();
+        Object prototype = serializer.deserializeInstance(tag.getTag("prototype"));
+        Object condition = serializer.deserializeCondition(tag.getTag("condition"));
+
+        return new PrototypedIngredient(component, prototype, condition);
+}
 
 }
