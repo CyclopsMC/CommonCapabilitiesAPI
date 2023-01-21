67b66c8b50399e5ef324d133e13275978e2ef495
Ruben Taelman <rubensworks@gmail.com>
2023-01-21 17:17:09 +0100

    Store reusable ingredient flags in recipes

--- capability/recipehandler/IRecipeDefinition.java
+++ capability/recipehandler/IRecipeDefinition.java
@@ -2,6 +2,7 @@ package org.cyclops.commoncapabilities.api.capability.recipehandler;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import net.minecraft.nbt.ByteArrayTag;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.nbt.ListTag;
@@ -43,6 +44,18 @@ public interface IRecipeDefinition extends Comparable<IRecipeDefinition> {
     public <T, M> List<IPrototypedIngredientAlternatives<T, M>> getInputs(IngredientComponent<T, M> ingredientComponent);
 
     /**
+     * If the input at the given index is reusable.
+     * If an ingredient is reusable, this means that a crafting job for this recipe will not (fully) consume this
+     * ingredient, and could potentially be reused in later crafting jobs.
+     * @param ingredientComponent An ingredient component type.
+     * @param index The index of an input, based on the order in {@link #getInputs(IngredientComponent)}.
+     * @param <T> The instance type.
+     * @param <M> The matching condition parameter, may be Void.
+     * @return If the input at this index is reusable.
+     */
+    public <T, M> boolean isInputReusable(IngredientComponent<T, M> ingredientComponent, int index);
+
+    /**
      * @return The output ingredients.
      */
     public IMixedIngredients getOutput();
@@ -55,18 +68,25 @@ public interface IRecipeDefinition extends Comparable<IRecipeDefinition> {
     public static CompoundTag serialize(IRecipeDefinition recipe) {
         CompoundTag tag = new CompoundTag();
         CompoundTag inputTag = new CompoundTag();
+        CompoundTag inputReusableTag = new CompoundTag();
         for (IngredientComponent<?, ?> component : recipe.getInputComponents()) {
             ListTag instances = new ListTag();
+            List<Byte> reusableBytes = Lists.newArrayList();
+            int index = 0;
             for (IPrototypedIngredientAlternatives ingredient : recipe.getInputs(component)) {
                 CompoundTag subTag = new CompoundTag();
                 IPrototypedIngredientAlternatives.ISerializer serializer = ingredient.getSerializer();
                 subTag.put("val", serializer.serialize(component, ingredient));
                 subTag.putByte("type", serializer.getId());
                 instances.add(subTag);
+                reusableBytes.add((byte) (recipe.isInputReusable(component, index) ? 1 : 0));
+                index++;
             }
             inputTag.put(component.getRegistryName().toString(), instances);
+            inputReusableTag.put(component.getRegistryName().toString(), new ByteArrayTag(reusableBytes));
         }
         tag.put("input", inputTag);
+        tag.put("inputReusable", inputReusableTag);
         tag.put("output", IMixedIngredients.serialize(recipe.getOutput()));
         return tag;
     }
@@ -79,12 +99,14 @@ public interface IRecipeDefinition extends Comparable<IRecipeDefinition> {
      */
     public static RecipeDefinition deserialize(CompoundTag tag) throws IllegalArgumentException {
         Map<IngredientComponent<?, ?>, List<IPrototypedIngredientAlternatives<?, ?>>> inputs = Maps.newIdentityHashMap();
+        Map<IngredientComponent<?, ?>, List<Boolean>> inputsReusable = Maps.newIdentityHashMap();
         if (!tag.contains("input")) {
             throw new IllegalArgumentException("A recipe tag did not contain a valid input tag");
         }
         if (!tag.contains("output")) {
             throw new IllegalArgumentException("A recipe tag did not contain a valid output tag");
         }
+
         CompoundTag inputTag = tag.getCompound("input");
         for (String componentName : inputTag.getAllKeys()) {
             IngredientComponent<?, ?> component = IngredientComponent.REGISTRY.getValue(new ResourceLocation(componentName));
@@ -116,8 +138,29 @@ public interface IRecipeDefinition extends Comparable<IRecipeDefinition> {
             }
             inputs.put(component, instances);
         }
+
+        if (tag.contains("inputReusable")) {
+            CompoundTag inputReusableTag = tag.getCompound("inputReusable");
+            for (String componentName : inputReusableTag.getAllKeys()) {
+                IngredientComponent<?, ?> component = IngredientComponent.REGISTRY.getValue(new ResourceLocation(componentName));
+                if (component == null) {
+                    throw new IllegalArgumentException("Could not find the ingredient component type " + componentName);
+                }
+                Tag subTag = inputReusableTag.get(componentName);
+                if (!(subTag instanceof ByteArrayTag instancesReusable)) {
+                    throw new IllegalArgumentException("The ingredient component type " + componentName + " did not contain a valid list of instance reusable bytes");
+                }
+                List<Boolean> inputReusable = Lists.newArrayList();
+                for (byte b : instancesReusable.getAsByteArray()) {
+                    inputReusable.add(b == (byte)1);
+                }
+                inputsReusable.put(component, inputReusable);
+            }
+        }
+
         IMixedIngredients output = IMixedIngredients.deserialize(tag.getCompound("output"));
-        return new RecipeDefinition(inputs, output);
+
+        return new RecipeDefinition(inputs, inputsReusable, output);
     }
 
 }
