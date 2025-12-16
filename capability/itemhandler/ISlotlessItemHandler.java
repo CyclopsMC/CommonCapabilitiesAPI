b78cdd90e43c6b897e48e86221442d803e24a950
Ruben Taelman <rubensworks@gmail.com>
2025-12-16 07:17:42 +0100

    Add getLimit TODO

--- capability/itemhandler/ISlotlessItemHandler.java
+++ capability/itemhandler/ISlotlessItemHandler.java
@@ -150,6 +150,6 @@ public interface ISlotlessItemHandler {
      *
      * @return The maximum item count allowed in the handler.
      */
-    int getLimit();
+    int getLimit(); // TODO: migrate to long in next major
 
 }
